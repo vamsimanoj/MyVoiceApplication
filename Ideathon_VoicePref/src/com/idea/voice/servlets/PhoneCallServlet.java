47d4ac9ee71849860742b09d6557aa5ae63616bb
vamsimanoj <vamsimanoj27@gmail.com>
2016-11-30 11:46:19 +0530

    Add files via upload

---
+++ Ideathon_VoicePref/src/com/idea/voice/servlets/PhoneCallServlet.java
@@ -0,0 +1,39 @@
+package com.idea.voice.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PhoneCallServlet
+ */
+public class PhoneCallServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PhoneCallServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.err.println("came here>???");
+		System.err.println(request.getParameter("callingNumber"));
+		response.sendRedirect("Home.jsp");
+	}
+
+}
